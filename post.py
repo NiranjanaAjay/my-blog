722ee8a50e74559be31c2eb5d40bb43bd56e9d98
Niranjana Ajay <ajayniranjana05@gmail.com>
2025-06-25 19:45:17 +0530

    templating done

--- post.py
+++ post.py
@@ -1,2 +1,6 @@
 class Post:
-    pass
+    def __init__(self,id_no,title,subtitle,body):
+        self.id_no = id_no
+        self.title = title
+        self.subtitle = subtitle
+        self.body = body
