bfe9a3feb0ebc8005d3a9bc80a52b20d473f3ba4
Niranjana Ajay <ajayniranjana05@gmail.com>
2025-07-14 20:53:38 +0530

    create add new post

--- blog_latest/main.py
+++ blog_latest/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
@@ -10,10 +10,14 @@ from flask_ckeditor import CKEditor, CKEditorField
 from datetime import date
 import os
 from dotenv import load_dotenv
+from datetime import datetime
+from flask_ckeditor import CKEditor
+
 
 load_dotenv()
 
 app = Flask(__name__)
+ckeditor = CKEditor(app)
 app.config['SECRET_KEY'] = os.environ["KEY"]
 Bootstrap5(app)
 
@@ -39,24 +43,59 @@ class BlogPost(db.Model):
 with app.app_context():
     db.create_all()
 
+class NewPost(FlaskForm):
+  title = StringField(label='Title of the blog', validators=[DataRequired()])
+  subtitle = StringField(label='Subtitle', validators=[DataRequired()])
+  body = CKEditorField(label='Content', validators=[DataRequired()])
+  author = StringField(label='Author', validators=[DataRequired()])
+  img = StringField(label='Image URL', validators=[URL()])
+  submit = SubmitField(label='Done')
+
 
 @app.route('/')
 def get_all_posts():
-    # TODO: Query the database for all the posts. Convert the data to a python list.
-    posts = []
+    posts = db.session.execute(db.select(BlogPost)).scalars().all()
     return render_template("index.html", all_posts=posts)
 
-# TODO: Add a route so that you can click on individual posts.
-@app.route('/')
+
+@app.route('/post/<int:post_id>')
 def show_post(post_id):
-    # TODO: Retrieve a BlogPost from the database based on the post_id
-    requested_post = "Grab the post from your database"
+    requested_post = db.session.execute(db.select(BlogPost).where(BlogPost.id==post_id)).scalar()
     return render_template("post.html", post=requested_post)
 
 
 # TODO: add_new_post() to create a new blog post
+@app.route('/add', methods=['GET','POST'])
+def add_new_post():
+    form = NewPost()
+    edit_post = False
+    if request.method == 'POST':
+        if request.form.get("img")=='':
+            image = ("https://images.unsplash.com/photo-1680299568009-09a6f8383061?q=80&w=1074&auto=format&fit"
+                     "=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+        else:
+            image = request.form.get("img")
+        new_post = BlogPost(
+            title = request.form.get("title"),
+            subtitle = request.form.get("subtitle"),
+            date = datetime.today().strftime("%d %B, %Y"),
+            body = request.form.get("body"),
+            author = request.form.get("author"),
+            img_url = image
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect("/")
+    return render_template("make-post.html", form=form, edit = edit_post)
+
 
 # TODO: edit_post() to change an existing blog post
+@app.route('/edit/<int:post_id>')
+def edit(post_id):
+    form = NewPost()
+    edit_post = True
+    requested_post = db.session.execute(db.select(BlogPost).where(BlogPost.id == post_id)).scalar()
+    return render_template("make-post.html", form=form, edit=edit_post)
 
 # TODO: delete_post() to remove a blog post from the database
 
